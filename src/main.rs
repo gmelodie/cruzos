37e34f802bdb4c79d16ec3d333df9feadfb56ccb
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-04-01 17:13:20 -0300

    Add working hello world version

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World";
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -17,7 +17,7 @@ pub extern "C" fn _start() -> ! {
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xa;
         }
     }
 
