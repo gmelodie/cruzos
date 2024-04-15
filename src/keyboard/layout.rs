c190e7709eab565a1f7975c5bc83d1d9ff8502ba
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-04-15 15:28:50 -0300

    Add keyboard module with layout support

---
+++ src/keyboard/layout.rs
@@ -0,0 +1,111 @@
+use crate::prelude::*;
+
+static UNDEFINED_KEY: char = '?'; // this is a block in code page 437
+
+#[derive(Debug, Clone, Copy)]
+pub enum KeyType {
+    ESC,
+    Ctrl,
+    Shift,
+    ShiftReleased,
+    Alt,
+    CapsLock,
+    ArrowUp,
+    ArrowDown,
+    ArrowLeft,
+    ArrowRight,
+    // TODO: backspace
+    Letter,
+    Unknown,
+}
+
+pub enum Layout {
+    Moonlander,
+}
+
+pub trait Layoutable {
+    fn to_keytype(&self, scancode: u8) -> KeyType;
+    fn to_ascii(&self, scancode: u8) -> char;
+}
+
+impl Layoutable for Layout {
+    fn to_keytype(&self, scancode: u8) -> KeyType {
+        match self {
+            Layout::Moonlander => moonlander_to_keytype(scancode)
+        }
+    }
+    fn to_ascii(&self, scancode: u8) -> char {
+        match self {
+            Layout::Moonlander => moonlander_to_ascii(scancode)
+        }
+    }
+}
+
+// MOONLANDER Layout
+fn moonlander_to_keytype(scancode: u8) -> KeyType {
+    log!(Level::Debug, "moonlander_to_keytype called with scancode {scancode}");
+    let letters = [
+        30, 48, 46, 32, 18, 33, 34, 35, 23, 36,
+        37, 38, 50, 49, 24, 25, 16, 19, 31, 20,
+        22, 47, 17, 45, 21, 44,
+        57, // space
+        28, // newline
+    ];
+
+    if letters.contains(&scancode) {
+        return KeyType::Letter; // also includes space and newline
+    }
+
+    match scancode {
+        1 => KeyType::ESC,
+        29 => KeyType::Ctrl,
+        42 => KeyType::Shift,
+        56 => KeyType::Alt,
+        72 => KeyType::ArrowUp,
+        80 => KeyType::ArrowDown,
+        77 => KeyType::ArrowLeft,
+        75 => KeyType::ArrowRight,
+        170 => KeyType::ShiftReleased,
+        58 => KeyType::CapsLock,
+        _ => KeyType::Unknown,
+    }
+}
+
+fn moonlander_to_ascii(scancode: u8) -> char {
+    log!(Level::Debug, "moonlander_to_ascii called with scancode {scancode}");
+    match scancode {
+        30 => 'a',
+        48 => 'b',
+        46 => 'c',
+        32 => 'd',
+        18 => 'e',
+        33 => 'f',
+        34 => 'g',
+        35 => 'h',
+        23 => 'i',
+        36 => 'j',
+        37 => 'k',
+        38 => 'l',
+        50 => 'm',
+        49 => 'n',
+        24 => 'o',
+        25 => 'p',
+        16 => 'q',
+        19 => 'r',
+        31 => 's',
+        20 => 't',
+        22 => 'u',
+        47 => 'v',
+        17 => 'w',
+        45 => 'x',
+        21 => 'y',
+        44 => 'z',
+        57 => ' ',
+        28 => '\n',
+        _ => UNDEFINED_KEY,
+    }
+}
+
+
+
+
---
+++ src/keyboard/mod.rs
@@ -0,0 +1,100 @@
+use x86_64::{
+    instructions::port::Port,
+    structures::idt::InterruptStackFrame,
+};
+use crate::{
+    prelude::*,
+    interrupts::{PICS, PICInterrupt},
+};
+
+
+mod layout;
+
+
+use layout::{KeyType, Layout, Layoutable};
+
+lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard> = Mutex::new(Keyboard::new()); // TODO: add layout to keyboard
+}
+
+
+struct Keyboard {
+    shift_pressed: bool,
+    caps_lock_on: bool,
+    layout: Layout,
+}
+
+impl Keyboard {
+    fn new() -> Keyboard {
+        Keyboard {
+            caps_lock_on: false,
+            shift_pressed: false,
+            layout: Layout::Moonlander,
+        }
+    }
+
+    fn caps_lock(&mut self) {
+        self.caps_lock_on = !self.caps_lock_on;
+    }
+
+    fn shift(&mut self) {
+        self.shift_pressed = true;
+    }
+
+    fn shift_released(&mut self) {
+        self.shift_pressed = false;
+    }
+
+    // see if we need to uppercase letter
+    fn to_ascii(&self, scancode: u8) -> char {
+        let ascii = self.layout.to_ascii(scancode);
+
+        match (self.caps_lock_on, self.shift_pressed) {
+            (true, true) => ascii,
+            (true, false) => ascii.to_ascii_uppercase(),
+            (false, true) => ascii.to_ascii_uppercase(),
+            (false, false) => ascii,
+        }
+
+    }
+}
+
+
+pub extern "x86-interrupt" fn keyboard_interrupt(_stack_frame: InterruptStackFrame) {
+    let scancode: u8 = unsafe {
+        let mut port = Port::new(0x60);
+        port.read()
+    };
+    log!(Level::Debug, "Got input from keyboard: {scancode}");
+
+    let keytype = KEYBOARD.lock().layout.to_keytype(scancode);
+    match keytype {
+        KeyType::Shift => KEYBOARD.lock().shift(),
+        KeyType::ShiftReleased => KEYBOARD.lock().shift_released(),
+        KeyType::CapsLock => KEYBOARD.lock().caps_lock(),
+        KeyType::Letter => {
+            let ascii = KEYBOARD.lock().to_ascii(scancode);
+            logf!(Level::Info, "{}", ascii);
+        }
+        KeyType::ESC => (),
+        KeyType::Ctrl => (),
+        KeyType::Alt => (),
+        KeyType::ArrowUp => (),
+        KeyType::ArrowDown => (),
+        KeyType::ArrowLeft => (),
+        KeyType::ArrowRight => (),
+        KeyType::Unknown => log!(Level::Warning, "Got unknown scancode {scancode}"), // do nothing
+        // TODO: backspace
+    }
+//     let ascii_key = scancode2ascii(key);
+//     logf!(Level::Info, "{ascii_key}"); // logf does not insert newlines
+    // TODO: keyboard buffer
+
+    unsafe {PICS.lock().notify_end_of_interrupt(PICInterrupt::Keyboard as u8)};
+}
+
+
+// fn scancode2ascii(scancode: u8) -> char {
+//     let sc2ascii = [ESC'a', 'b'];
+//     'a'
+// }
